4c740b9c07921b079c02ec3f821b1a00f3e23952
alireza <alireza.f.7596@gmail.com>
2024-08-31 19:32:04 +0330

    start sslkeeper djnago

---
+++ sslkeeper/celery.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from celery.schedules import crontab
+
+# Set default Django settings module for 'celery' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sslkeeper.settings')
+
+# Create an instance of the Celery application.
+app = Celery('sslkeeper')
+
+# Configure Celery using Django settings.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Auto-discover tasks from all registered Django app configs.
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'check-certificate-expiry-every-day': {
+        'task': 'domains.tasks.check_certificate_expiry',
+        'schedule': crontab(hour=0, minute=0),  # Run once a day at midnight
+    },
+}
--- sslkeeper/settings.py
+++ sslkeeper/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'domains',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'sslkeeper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DB_NAME', 'sslkeeper'),
+        'USER': os.environ.get('DB_USER', 'user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'userpassword'),
+        'HOST': os.environ.get('DB_HOST', 'mariadb'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
@@ -121,3 +128,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# # Telegram Configuration
+# TELEGRAM_BOT_TOKEN = '7462881009:AAHxfX9dyDL2-xSt7dxUv56_iItzaYj3KUc'
+# TELEGRAM_CHAT_ID = '-1002148862476'
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN', '')
+TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID', '')
+
+# # Celery Configuration Options
+# CELERY_BROKER_URL = 'redis://redis:6379/0'
+# CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
+CELERY_ACCEPT_CONTENT = os.environ.get('CELERY_ACCEPT_CONTENT', ['json'])
+CELERY_TASK_SERIALIZER = os.environ.get('CELERY_TASK_SERIALIZER', 'json')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
