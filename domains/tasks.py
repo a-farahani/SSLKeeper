16cfcf186d8af5ad2b2d1c3f6126446b23bb5980
a-farahani <alireza.f.7596@gmail.com>
2024-09-07 11:53:30 +0330

    add def get_certificates_cloudflare

--- domains/tasks.py
+++ domains/tasks.py
@@ -44,49 +44,8 @@ def generate_certificate(domain_id):
     """
     Generate the SSL certificate using Certbot and Cloudflare DNS challenge.
     """
-
     domain = Domain.objects.get(id=domain_id)
-    domain_name = domain.domain_name
-    email = domain.email
-    api_key = domain.cloudflare_api_key.api_key
-
-    # Create a temporary credentials file for this specific renewal
-    cloudflare_credentials_path = f"/tmp/cloudflare_{domain_name}.ini"
-    
-    with open(cloudflare_credentials_path, 'w') as f:
-        f.write(f'dns_cloudflare_api_token = {api_key}')
-
-    # Ensure the credentials file is not world-readable
-    os.chmod(cloudflare_credentials_path, 0o600)
-
-    # Command to run Certbot with DNS challenge using dynamic Cloudflare credentials
-    certbot_command = [
-        'certbot', 'certonly',
-        '-d', domain_name,
-        '-d', f'*.{domain_name}',
-        '--dns-cloudflare',
-        '--dns-cloudflare-credentials', cloudflare_credentials_path,
-        '--non-interactive',
-        '--agree-tos',
-        '--email', email,
-        '--force-renewal'
-    ]
-
-    if not domain.certificate:
-        try:
-            subprocess.run(certbot_command, check=True)
-            print(f"Successfully renewed certificate for {domain_name}")
-
-            # Update the domain model with new certificate information
-            update_domain_certificates(domain)
-
-        except subprocess.CalledProcessError as e:
-            print(f"Failed to renew certificate for {domain_name}: {e}")
-        
-        finally:
-            # Clean up credentials file
-            if os.path.exists(cloudflare_credentials_path):
-                os.remove(cloudflare_credentials_path)
+    get_certificates_cloudflare(domain)
 
 
 @shared_task
@@ -109,6 +68,39 @@ def renew_certificate(domain):
     """
     Renew the SSL certificate using Certbot and Cloudflare DNS challenge.
     """
+    get_certificates_cloudflare(domain)
+
+
+def update_domain_certificates(domain):
+    """
+    Update the Domain model instance with new certificate details.
+    """
+    # Define paths to Let's Encrypt certificate files for this domain
+    cert_path = f'/etc/letsencrypt/live/{domain.domain_name}/cert.pem'
+    privkey_path = f'/etc/letsencrypt/live/{domain.domain_name}/privkey.pem'
+    chain_path = f'/etc/letsencrypt/live/{domain.domain_name}/chain.pem'
+    fullchain_path = f'/etc/letsencrypt/live/{domain.domain_name}/fullchain.pem'
+
+    try:
+        with open(cert_path, 'r') as cert_file:
+            domain.certificate = cert_file.read()
+
+        with open(privkey_path, 'r') as privkey_file:
+            domain.private_key = privkey_file.read()
+
+        with open(chain_path, 'r') as chain_file:
+            domain.chain = chain_file.read()
+
+        with open(fullchain_path, 'r') as fullchain_file:
+            domain.fullchain = fullchain_file.read()
+
+        domain.save()
+        print(f"Updated domain model with new certificate details for {domain.domain_name}")
+
+    except FileNotFoundError as e:
+        print(f"Error updating certificates for {domain.domain_name}: {e}")
+
+def get_certificates_cloudflare(domain):
     domain_name = domain.domain_name
     email = domain.email
     api_key = domain.cloudflare_api_key.api_key
@@ -149,33 +141,3 @@ def renew_certificate(domain):
         # Clean up credentials file
         if os.path.exists(cloudflare_credentials_path):
             os.remove(cloudflare_credentials_path)
-
-
-def update_domain_certificates(domain):
-    """
-    Update the Domain model instance with new certificate details.
-    """
-    # Define paths to Let's Encrypt certificate files for this domain
-    cert_path = f'/etc/letsencrypt/live/{domain.domain_name}/cert.pem'
-    privkey_path = f'/etc/letsencrypt/live/{domain.domain_name}/privkey.pem'
-    chain_path = f'/etc/letsencrypt/live/{domain.domain_name}/chain.pem'
-    fullchain_path = f'/etc/letsencrypt/live/{domain.domain_name}/fullchain.pem'
-
-    try:
-        with open(cert_path, 'r') as cert_file:
-            domain.certificate = cert_file.read()
-
-        with open(privkey_path, 'r') as privkey_file:
-            domain.private_key = privkey_file.read()
-
-        with open(chain_path, 'r') as chain_file:
-            domain.chain = chain_file.read()
-
-        with open(fullchain_path, 'r') as fullchain_file:
-            domain.fullchain = fullchain_file.read()
-
-        domain.save()
-        print(f"Updated domain model with new certificate details for {domain.domain_name}")
-
-    except FileNotFoundError as e:
-        print(f"Error updating certificates for {domain.domain_name}: {e}")
